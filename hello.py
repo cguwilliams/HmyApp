a2dcde9cbc94a6e0e4768557b2109c3ca3346524
pbwilliams <pbwilliams@uchicago.edu>
2015-04-09 21:25:38 -0500

    heroku commit

--- hello.py
+++ hello.py
@@ -1,16 +1,8 @@
 from flask import Flask
-import numpy
-import matplotlib
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World! Hello Github'
-
-def wave():
-    x = linspace(0, 10)
-    y = sin(x)
-    plot(x, y)
     
-wave()
