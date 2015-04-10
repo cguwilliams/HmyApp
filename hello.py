f03172bebea32e0fe7abd11c24c367cd24ce3de8
pbwilliams <pbwilliams@uchicago.edu>
2015-04-09 20:28:53 -0500

    second commit

--- hello.py
+++ hello.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World! Hello Github'
+
+map = Basemap(projection='ortho', lat_0=50, lon_0=-100,
+              resolution='l', area_thresh=1000.0)
+ 
+map.drawcoastlines()
+ 
+plt.show()
